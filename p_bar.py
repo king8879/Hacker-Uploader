c3de9da6fb96faa2eec0143353e85391d0450743
king8879 <k4507244@gmail.com>
2025-05-01 18:04:15 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -77,7 +77,7 @@ async def progress_bar(current, total, reply, start):
         else:
             perc = f"{current * 100 / total:.1f}%"
             elapsed_time = round(diff)
-            speed = current*3 / elapsed_time
+            speed = current / elapsed_time
             remaining_bytes = total - current
             if speed > 0:
                 eta_seconds = remaining_bytes / speed
